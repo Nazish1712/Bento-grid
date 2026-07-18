3f60b949b93b51b60b5e939983dc817edbe4007e
Nazish1712 <nazish1712@gmail.com>
2026-07-18 23:32:49 +0530

    started creating with header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-dvh w-full max-w-4xl mx-auto pt-20 pb-10">
+      <Header/>
+     </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h1 className="text-4xl font-bold tracking-tighter text-neutral-700">Bento grid are cool, you should try it sometimes</h1>
+    <p className="text-base text-neutral-500 max-w-xl mt-4">Discover innovative solutions that transform the way work and create. Our cutting edge tools are designed to empower your creativity and boost your productivity like never before.</p>
+   </>
+  )
+}
+
+export default Header
