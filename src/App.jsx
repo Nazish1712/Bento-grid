83129c27007e7f082f7cbdc8f06ece8c09941163
Nazish1712 <nazish1712@gmail.com>
2026-07-19 23:49:18 +0530

    added image card

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <div className="h-dvh w-full max-w-4xl mx-auto pt-20 pb-10">
       <Header/>
+      <div className="grid grid-cols-4 gap-4 mt-10">
+      <Column>
+        <Card></Card>
+        <Card></Card>
+      </Column>
+      </div>
      </div>
   )
 }
